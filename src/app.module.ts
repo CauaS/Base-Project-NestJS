e3619b10ed61cf0e4860c6c58ce428ead9ddcff7
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-06-26 13:41:46 -0300

    chore: sequelize config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
 import { AppController } from './controllers/app.controller';
 import { ProdutosController } from './controllers/produtos.controller';
+import { LivroModel } from './model/livro.model';
 import { LivroService } from './services/livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([LivroModel]),
+  ],
   controllers: [AppController, ProdutosController],
   providers: [AppService, LivroService],
 })
