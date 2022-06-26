1da878e5ee1645eb8e677976469d41f340ad503f
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-06-26 12:54:36 -0300

    chore: added controller into a new foler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
 import { ProdutosController } from './produtos.controller';
 import { ProdutosService } from './services/produtos.service';
 
