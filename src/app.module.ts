70a2e3a088ee9c2edaf28ad519cffc6105e4d095
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-06-26 12:52:10 -0300

    chore: addedd new service to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosController } from './produtos.controller';
+import { ProdutosService } from './services/produtos.service';
 
 @Module({
-    imports: [],
-    controllers: [AppController, ProdutosController],
-    providers: [AppService],
+  imports: [],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosService],
 })
 export class AppModule {}
